f97a14ca12c66d0d70539d9bf5ff846db4efd64a
mishita1 <mishita.1@unthinkable.co>
2025-06-25 14:17:19 +0530

    fixed

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,5 +1,4 @@
 import { Socials } from "@/Constants";
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-"use client"
-
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -12,10 +9,10 @@ export default function Home() {
       >
         <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">
-           Hi, I am
+            Hi, I am
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
               {" "}
-         Mishita Arora
+              Mishita Arora
             </span>
           </h1>
           <p className="text-gray-200 hidden md:block">
@@ -27,7 +24,7 @@ export default function Home() {
               className="rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
             >
               Learn more
-    </Link>
+            </Link>
           </div>
         </div>
       </div>
@@ -39,12 +36,7 @@ export default function Home() {
         >
           Learn more
         </Link>
-
-     
       </div>
-
-
-
     </main>
   );
 }
